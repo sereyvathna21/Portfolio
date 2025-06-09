c8f507bd0f8c6c4e2aee7b111b02630b600686d7
sereyvathna21 <vattdy192@gmail.com>
2025-06-09 16:19:19 +0700

    PWA

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "pwa-notepad-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./css/styles.css",
+  "./css/tailwind.css",
+  "./images/Em_Sereyvathna-CV.png",
+  "./images/Profile.jpeg",
+  "tailwind.config.js",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});

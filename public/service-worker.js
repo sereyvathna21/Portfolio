9af6062c6c41e12ddfd0de027af32733c810a296
sereyvathna21 <vattdy192@gmail.com>
2025-06-10 10:47:51 +0700

    Finish

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,12 @@
-const CACHE_NAME = "pwa-cahce-v2";
+const CACHE_NAME = "pwa-cache-v2";
 const FILES_TO_CACHE = [
   "./",
-  "./public/index.html",
-  "./public/css/styles.css",
-  "./public/css/tailwind.css",
-  "./public/images/Em_Sereyvathna-CV.png",
-  "./public/images/Profile.jpeg",
-  "./public/tailwind.config.js",
+  "./index.html",
+  "./css/styles.css",
+  "./css/tailwind.css",
+  "./images/Em_Sereyvathna-CV.png",
+  "./images/Profile.jpeg",
+  "./tailwind.config.js",
 ];
 
 self.addEventListener("install", (event) => {
